f2f8106c4957ec1c90f6eb2e768ebb9833eed3b0
SalmanShams67 <salman@thekites.ai>
2024-03-08 19:22:52 +0530

    commit

---
+++ C++/1_Basics/4_updation.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 5;
+    cout << x << endl;
+
+    x = x + 4; // x+=4, x++
+    cout << x << endl;
+}
---
+++ C++/2_Maths/1_operators.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float x = 11, y = 5; // int
+    cout << x + y << endl;
+    cout << x - y << endl;
+    cout << x * y << endl;
+    cout << x / y << endl; // int 2, float 2.2
+}
+
+// int = integer (...... -2, -1, 0, 1, 2.....)
+// float = real num (...... -2, .., -1.5,  0,  .., 1.5......)
