ec33acde96590a8a6391d54412db5a07c8584478
SalmanShams67 <salman@thekites.ai>
2024-03-08 11:41:00 +0530

    hi

---
+++ New folder/1_Basics/1_print.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Yo, what's up?" << endl;
+    cout << endl
+         << "Hi!!";
+    cout << "\n";
+    cout << "How u doing?";
+}
+
+// hi
---
+++ New folder/1_Basics/3_variables.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i;
+    i = 5;
+
+    int j = 4, k = 2;
+
+    cout << (i + j) * k;
+}
+// hi
