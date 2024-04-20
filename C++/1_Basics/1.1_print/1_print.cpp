4eeb356bc67c620e2486b53437746ef29d7ec08a
Salmanshams67 <salman@thekites.ai>
2024-04-20 17:13:04 +0530

    20/04/2024

---
+++ C++/1_Basics/1.1_print/1_print.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Yo, what's up?" << endl;
+    cout << endl
+         << "Hi!!";
+    cout << "\n";
+    cout << "How u doing?";
+}
+
+// 1. #include <iostream>: This line includes the input-output stream library (iostream),
+// which allows you to perform input and output operations in C++.
+
+// 2. using namespace std;: This line declares that the code will be using the std namespace.
+// The std namespace contains all the standard C++ functions and objects, including those for input and output.
+
+// 3. int main(): This line declares the main function of the program. In C++, every program must have a main function,
+// and execution of the program starts from here.
