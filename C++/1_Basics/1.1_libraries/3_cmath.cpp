ad114951b837af8f41ea3bfa5dcf40eb001bcb8f
Salmanshams67 <salman@thekites.ai>
2024-04-20 16:49:15 +0530

    20/04/2024

---
+++ C++/1_Basics/1.1_libraries/3_cmath.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    // Calculating the square root of a number
+    double number = 25.0;
+    double squareRoot = sqrt(number);
+    cout << "Square root of " << number << " is: " << squareRoot << endl;
+
+    // // Calculating the value of sine function
+    // double angle = 45.0;                          // Angle in degrees
+    // double sineValue = sin(angle * M_PI / 180.0); // Convert angle to radians before using sin function
+    // cout << "Sine of " << angle << " degrees is: " << sineValue << endl;
+
+    return 0;
+}
