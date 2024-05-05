32889a072735a02d9ee0aed85a7e7d4c299f67ac
Salmanshams67 <salman@thekites.ai>
2024-05-05 16:52:42 +0530

    5/5/24

---
+++ C++/14_file/1_.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Yo, what's up?" << endl;
+    cout << endl
+         << "Hi!!";
+    cout << "\n";
+    cout << "How u doing?";
+}
