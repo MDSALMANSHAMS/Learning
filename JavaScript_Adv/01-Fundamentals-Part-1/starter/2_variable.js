972e90b798bd37b742eec567db7cd7c9d6011987
MDSALMANSHAMS <salmanshams67@gmail.com>
2024-05-07 09:51:35 +0530

    7/5/24

---
+++ JavaScript_Adv/01-Fundamentals-Part-1/starter/2_variable.js
@@ -0,0 +1,14 @@
+let js = 66;
+console.log(js);
+
+// Q.
+// Declare variables called country, continent and population and
+// assign their values according to your own country (population in millions).
+
+let country = "India";
+let continent = "Asia";
+let population = 140;
+
+console.log(country);
+console.log(continent);
+console.log(population);
---
+++ JavaScript_Adv/01-Fundamentals-Part-1/starter/3_dataType.js
@@ -0,0 +1,21 @@
+// 1. number
+let age = 23;
+
+// 2. string
+let my_name = "Salman shams";
+
+// 3. boolean
+let vaccination = true;
+
+// 4. undefined
+let children;
+console.log(typeof children);
+
+children = "two";
+console.log(typeof children);
+
+// 5. null
+
+// 6. symbol
+
+// 7. bigint
