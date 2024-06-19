3351e36cff398476159e7e9b05c884763092fa05
MDSALMANSHAMS <salmanshams67@gmail.com>
2024-06-19 11:37:28 +0530

    Normalize line endings

---
+++ build/CMakeFiles/C++_imageProcessing.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "C++_imageProcessing.exe"
+  "C++_imageProcessing.exe.manifest"
+  "C++_imageProcessing.pdb"
+  "CMakeFiles/C++_imageProcessing.dir/src/main.cpp.obj"
+  "CMakeFiles/C++_imageProcessing.dir/src/main.cpp.obj.d"
+  "libC++_imageProcessing.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C++_imageProcessing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
