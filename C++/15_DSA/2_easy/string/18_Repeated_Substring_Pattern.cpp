adc694a48157eef4c0430db0862f474f745a6068
MDSALMANSHAMS <salmanshams67@gmail.com>
2024-08-01 12:42:37 +0530

    1/8

---
+++ C++/15_DSA/2_easy/string/18_Repeated_Substring_Pattern.cpp
@@ -0,0 +1,34 @@
+/*
+Given a string s, check if it can be constructed by taking a substring of it and
+appending multiple copies of the substring together.
+
+Example 1:
+Input: s = "abab"
+Output: true
+Explanation: It is the substring "ab" twice.
+
+Example 2:
+Input: s = "aba"
+Output: false
+
+Example 3:
+Input: s = "abcabcabcabc"
+Output: true
+Explanation: It is the substring "abc" four times or the substring "abcabc" twice.
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool isRepeting(string s)
+{
+}
+
+int main()
+{
+    string s = "abab";
+
+    cout << isRepeting(s);
+    return 0;
+}
