516070df7102da4f41c3c10b0da98d5491eb4141
MDSALMANSHAMS <salmanshams67@gmail.com>
2024-08-23 16:32:32 +0530

    23/8

--- C++/15_DSA/3_medium/array/3_rotate.cpp
+++ C++/15_DSA/3_medium/array/3_rotate.cpp
@@ -3,13 +3,41 @@
 #include <algorithm> // For sort
 using namespace std;
 
-void reverse(int arr[], int i, int j)
+// void reverse(int arr[], int i, int j)
+// {
+//     while (i < j)
+//     {
+//         int temp = arr[i];
+//         arr[i] = arr[j];
+//         arr[j] = temp;
+//         i++;
+//         j--;
+//     }
+// }
+
+// int main()
+// {
+//     int arr[] = {1, 2, 3, 4};
+//     int length = sizeof(arr) / 4;
+//     int k = 2;
+//     int rem = k % length;
+
+//     reverse(arr, 0, length - rem - 1);
+//     reverse(arr, length - rem, length - 1);
+//     reverse(arr, 0, length - 1);
+
+//     cout << "arr: ";
+//     for (int i = 0; i < length; i++)
+//     {
+//         cout << arr[i] << "  ";
+//     }
+// }
+
+void reverse(vector<int> &arr, int i, int j)
 {
     while (i < j)
     {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+        swap(arr[i], arr[j]);
         i++;
         j--;
     }
@@ -17,18 +45,21 @@ void reverse(int arr[], int i, int j)
 
 int main()
 {
-    int arr[] = {1, 2, 3, 4};
-    int length = sizeof(arr) / 4;
+    vector<int> arr = {1, 2, 3, 4, 5};
+    int length = arr.size();
     int k = 7;
     int rem = k % length;
+
+    // cout << rem << endl;
+    // cout << 10 % 5 << endl;
 
     reverse(arr, 0, length - rem - 1);
     reverse(arr, length - rem, length - 1);
     reverse(arr, 0, length - 1);
 
-    cout << "arr: ";
+    cout << "array: ";
     for (int i = 0; i < length; i++)
     {
-        cout << arr[i] << "  ";
+        cout << arr[i] << " ";
     }
 }
