e5330472de772b1a183ffc37a0e8e79cbdb04186
MDSALMANSHAMS <salmanshams67@gmail.com>
2024-12-09 14:15:47 +0530

    09/12

--- C++/15_DSA/2_easy/array/28_Pascal's_Triangle.cpp
+++ C++/15_DSA/2_easy/array/28_Pascal's_Triangle.cpp
@@ -18,6 +18,38 @@ using namespace std;
 
 int main()
 {
+    int numRows = 10;
+    vector<vector<int>> ans(numRows);
+    ans[0].push_back(1);
+
+    for (int i = 1; i < numRows; i++)
+    {
+        ans[i].push_back(1);
+
+        for (int j = 1; j < i; j++)
+        {
+            int num = ans[i - 1][j - 1] + ans[i - 1][j];
+            ans[i].push_back(num);
+        }
+
+        ans[i].push_back(1);
+    }
+
+    for (int i = 0; i < ans.size(); i++)
+    {
+        for (int j = 0; j < ans[i].size(); j++)
+        {
+            cout << ans[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+/*
+int main()
+{
     int numRows = 4;
     vector<vector<int>> ans(numRows);
     ans[0].push_back(1);
@@ -46,3 +78,5 @@ int main()
         cout << endl;
     }
 }
+
+*/
